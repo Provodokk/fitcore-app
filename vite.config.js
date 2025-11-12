39c312ec6262105cdd2d1865161b35bdd4896e23
Alexey <renal.hakimov@gmail.com>
2025-11-12 12:42:02 +0300

    Fix build for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+  build: {  
+    server: {
     port: 3000,
   },
-  build: {
+
     outDir: 'dist',
   }
 })
